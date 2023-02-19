92d2282a2df960391cfd90ea54ec8cf56fbc7f2c
dhirajk1994 <dhirajk1994@gmail.com>
2023-02-19 15:49:36 +0530

    sample

--- src/main/java/WPAPI/App.java
+++ src/main/java/WPAPI/App.java
@@ -1,20 +1,5 @@
 package WPAPI;
 
-import static io.restassured.RestAssured.given;
-
-import java.io.IOException;
-import java.util.ArrayList;
-
-import org.apache.commons.lang3.StringUtils;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
-import Resources.StringResources;
-import io.restassured.RestAssured;
-import static io.restassured.RestAssured.*;
-
-import io.restassured.path.json.JsonPath;
 
 /**
  * Hello world!
@@ -25,22 +10,8 @@ public class App
     public static void main( String[] args ) throws Exception
     {
     	
-    	String name="Priyanka Dhiraj Kulkarni";
-    
-    	String firstName=name.substring(0, 8);
-    	System.out.println(firstName);
     	
-    	String middlename=StringUtils.substringBetween(name, " ", " ");
-    	System.out.println(middlename);
-    	/*
-    	String testName="Device1_FM_Test";
-    	String deviceName=testName.substring(0, 7);
-    	String whatsAppAccount= StringUtils.substringBetween(testName, "_", "_") + " WhatsApp";
-
-		LogMethods.printOnConsole("🔥 Device1_FM_Test Started 🔥");
-		LogMethods.printOnConsole("🔥 "+deviceName+" 🔥");
-		LogMethods.printOnConsole("🔥 "+whatsAppAccount+" 🔥");
-*/
+    	
     } 
     
     	
