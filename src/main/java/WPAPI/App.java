1bf0bbb060936dc9730f7a5767c92176287af5ec
dhirajk1994 <dhirajk1994@gmail.com>
2023-03-03 18:50:56 +0530

    GroupsMania

--- src/main/java/WPAPI/App.java
+++ src/main/java/WPAPI/App.java
@@ -1,5 +1,14 @@
 package WPAPI;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import com.github.siyoon210.ogparser4j.OgParser;
+
 
 /**
  * Hello world!
@@ -9,12 +18,86 @@ public class App
 {
     public static void main( String[] args ) throws Exception
     {
+    	String link = null;
+    	String category;
+    	String language;
+    	String country;
+    	int count=93074; //Already defined
+    	
+    	try{    
+
+    		
+    		
+    		Connection con=DriverManager.getConnection(  
+    		"jdbc:mysql://89.117.53.127:3306/groupsmania","GroupsMania","Ads@40000");  
+    		//here sonoo is database name, root is username and password  
+    		Statement stmt=con.createStatement();  
+    		ResultSet rs=stmt.executeQuery("SELECT *  FROM groups");
+
+    		int i=0;
+    		int valid_groups=0;
+    		while(rs.next())  
+        	
+    		{
+    			i++;
+    			link=rs.getString(5);
+    			category=rs.getNString(7);
+    			country=rs.getNString(8);
+    			language=rs.getString(9);
+
+
+        		OgParser ogParser = new OgParser();
+        		com.github.siyoon210.ogparser4j.OpenGraph openGraph = ogParser.getOpenGraphOf("https://chat.whatsapp.com/"+link);
+        		String name=openGraph.getContentOf("title").getValue();    	
+        		String image=openGraph.getContentOf("image").getValue();    	
+        		
+
+        		if(!image.contains("static.whatsapp.net")) {        		
+    			    
+    			    //Query To Insert into Updated DB
+        		
+        			
+        			try {
+        			
+    	    		Statement update_statement=con.createStatement();  
+    	    		int rs_update=update_statement.executeUpdate("INSERT INTO updated_groups(link, name, category, country, language, image) VALUES ('"+link+"','"+name+"','"+category+"','"+country+"','"+language+"','"+image+"')");
+
+    	    		valid_groups++;
+        			}
+        			catch(Exception e) {
+        	    		System.out.println(e.getMessage());
+        	    		continue;
+        			}
+        			
+        		
+        		}
+    			System.out.println("****************************************");
+    			System.out.println("Completed " + i +"  Of " +count +"  Group Links");
+    			System.out.println("Valid Groups " + valid_groups);
+
+    			System.out.println("****************************************");
+
+        	}
+			con.close();  
+
+    		
+    	    	
+    		}
+    	catch(Exception e){ 
+    		
+    		System.out.println(e.getMessage());
+    	
+    	}  
+    		
+    		 
+    		
+		}  
     	
     	
-    	
+    		     	
     } 
     
     	
     	
     
-}
+
--- src/test/java/AllTestCases/demoTests.java
+++ src/test/java/AllTestCases/demoTests.java
@@ -7,6 +7,7 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
+import WPAPI.GroupsManiaLogic;
 import WPAPI.LogMethods;
 
 public class demoTests {
@@ -18,46 +19,7 @@ public class demoTests {
 	@Test
 	public void Test1() {
 		
-		extent.attachReporter(spark);
-		test=extent.createTest("Test1").assignCategory("Device 1");
-		LogMethods.DemoMethod(extent, test);
-		
+		GroupsManiaLogic.logic();
 	}
-	@Test
-	public void Test2() {
-		
-		test=extent.createTest("Test2").assignCategory("Device 2");
-		LogMethods.DemoMethod(extent, test);
-
-		
-	}
-	@Test
-	public void Test3() {
-		
-		test=extent.createTest("Test3").assignCategory("Device 3");
-
-		LogMethods.DemoMethod(extent, test);
-
-	}
-	@Test
-	public void Test4() {
-		
-		test=extent.createTest("Test4").assignCategory("Device 4");
-		LogMethods.DemoMethod(extent, test);
-
-		
-	}
-	@Test
-	public void Test5() {
-		
-		test=extent.createTest("Test5").assignCategory("Device 5");
-		LogMethods.DemoMethod(extent, test);
-
-		
-	}
-	@AfterSuite
-	public void flush() {
-		extent.flush();
-		
-	}
+	
 }
